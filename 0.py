7b6b8a305a027008c039dbebdfb3e12771d79d27
Fenfiriy <stephan.petrovvv@gmail.com>
2020-12-24 13:12:54 +0300

    added forward message option

--- 0.py
+++ 0.py
@@ -10,15 +10,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # from bs4 import BeautifulSoup
 
 url = 'https://10minutemail.com'
-# driver = webdriver.Edge('C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
-# soup = BeautifulSoup(driver.page_source, 'html.parser')
-# print(soup.prettify())
 client = discord.Client()
 drivers = dict()
 sess_starts = dict()
 opts = webdriver.firefox.options.Options()
-# opts.binary_location = r'C:\Users\steph\AppData\Local\Programs\Opera GX\launcher.exe'
-# opts.headless = True
+opts.headless = True
 
 
 @client.event
@@ -29,7 +25,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     user = message.author
-    if user in drivers and sess_starts[user] + 900 > time.time():
+    if user in drivers and sess_starts[user] + 900 < time.time():
         del drivers[user]
         del sess_starts[user]
         await message.channel.send('Your session expired')
@@ -61,46 +57,66 @@ async def on_message(message):
                             ' | Date: ' + msg.find_element_by_class_name('small_date').text + '\n'
                 await message.channel.send('You have ' + str(msgs_count) + ' messages in mailbox' + preview)
 
-            elif message.content.startswith('$read '):
-                try:
-                    msg_index = int(message.content[6:])
-                except ValueError:
-                    await message.channel.send('Unsupported index')
-                else:
-                    if msg_index > msgs_count or msg_index <= 0:
-                        await message.channel.send('The message with that index does not exist, you only have ' +
-                                                   str(msgs_count) + ' messages')
-                    else:
-                        msg = messages[msg_index - 1]
-                        print(msg.find_element_by_class_name('message_bottom').is_displayed())
-                        if not msg.find_element_by_class_name('message_bottom').is_displayed():
-                            msg.click()
-                        txt_to_print = msg.find_element_by_class_name('message_bottom').text
-                        await message.channel.send(txt_to_print)
-
-            elif message.content.startswith('$reply_to '):
-                try:
-                    msg_index = int(message.content.split()[1])
-                except ValueError:
-                    await message.channel.send('Unsupported index')
-                else:
-                    if msg_index > msgs_count or msg_index <= 0:
-                        await message.channel.send('The message with that index does not exist')
-                    else:
-                        msg = messages[msg_index - 1]
-                        if not msg.find_element_by_class_name('message_bottom').is_displayed():
-                            await message.channel.send('Maybe you should read it first?')
-                        else:
-                            text_to_reply = "".join(message.content.split()[2:])
-                            print(text_to_reply)
-                            msg.find_element_by_class_name('message_reply_icon').click()
-                            msg.find_element_by_class_name('reply_message_text').send_keys(text_to_reply)
-                            msg.find_element_by_class_name('reply_message_submit').click()
-
             elif message.content == '$end':
                 driver.quit()
                 del drivers[user]
                 await message.channel.send('Your session ended')
+
+            else:
+                msg_split = message.content.split(maxsplit=2)
+                if msg_split[0] == '$read':
+                    try:
+                        msg_index = int(msg_split[1])
+                    except ValueError:
+                        await message.channel.send('Unsupported index')
+                    else:
+                        if msg_index > msgs_count or msg_index <= 0:
+                            await message.channel.send('The message with that index does not exist, you only have ' +
+                                                       str(msgs_count) + ' messages')
+                        else:
+                            msg = messages[msg_index - 1]
+                            print(msg.find_element_by_class_name('message_bottom').is_displayed())
+                            if not msg.find_element_by_class_name('message_bottom').is_displayed():
+                                msg.click()
+                            txt_to_print = msg.find_element_by_class_name('message_bottom').text
+                            await message.channel.send(txt_to_print)
+
+                elif msg_split[0] == '$reply_to':
+                    try:
+                        msg_index = int(msg_split[1])
+                    except ValueError:
+                        await message.channel.send('Unsupported index')
+                    else:
+                        if msg_index > msgs_count or msg_index <= 0:
+                            await message.channel.send('The message with that index does not exist')
+                        else:
+                            msg = messages[msg_index - 1]
+                            if not msg.find_element_by_class_name('message_bottom').is_displayed():
+                                await message.channel.send('Maybe you should read it first?')
+                            else:
+                                text_to_reply = msg_split[2]
+                                print(text_to_reply)
+                                msg.find_element_by_class_name('message_reply_icon').click()
+                                msg.find_element_by_class_name('reply_message_text').send_keys(text_to_reply)
+                                msg.find_element_by_class_name('reply_message_submit').click()
+
+                elif msg_split[0] == '$forward':
+                    try:
+                        msg_index = int(msg_split[1])
+                    except ValueError:
+                        await message.channel.send('Unsupported index')
+                    else:
+                        if msg_index > msgs_count or msg_index <= 0:
+                            await message.channel.send('The message with that index does not exist')
+                        else:
+                            msg = messages[msg_index - 1]
+                            if not msg.find_element_by_class_name('message_bottom').is_displayed():
+                                await message.channel.send('Maybe you should read it first?')
+                            else:
+                                forward_address = msg_split[2]
+                                msg.find_element_by_class_name('message_forward_icon').click()
+                                msg.find_element_by_class_name('forward_message_address').send_keys(forward_address)
+                                msg.find_element_by_class_name('forward_message_submit').click()
         else:
             await message.channel.send('Your session does not exist')
 
